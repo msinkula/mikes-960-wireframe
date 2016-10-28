e7caa7c9cf7b994294ac82d4566b4a0044d3be38
Mike Sinkula <mike@mikesinkula.com>
2016-10-28 15:46:22 -0700

    You are welcome Anna.

--- front-page.php
+++ front-page.php
@@ -18,10 +18,28 @@
     <?php endwhile; ?>
     <?php endif; ?>
     <!-- End Content Loop -->
-
+    
     <!-- Begin News Loop -->
+    <small>Latest News:</small>
     <?php rewind_posts(); // stop loop one ?>
     <?php query_posts('showposts=5'); // show 5 latest posts ?>
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article class="post-box" id="post-box-<?php the_ID(); ?>">
+            <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <p class="postdata">Posted on <?php the_time('M j, Y') ?> in <?php the_category(', ') ?></p>
+            <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+            <?php the_excerpt(); ?>
+            <p class="full-story"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">Full Story &raquo;</a></p>
+        </article>
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <!-- End News Loop -->
+    
+    <!-- Begin News Loop -->
+    <small>Latest Productions:</small>
+    <?php rewind_posts(); // stop loop one ?>
+    <?php query_posts('post_type=new_production'); // show productions ?>
     <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
         <article class="post-box" id="post-box-<?php the_ID(); ?>">
